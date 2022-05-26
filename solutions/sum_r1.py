774c9cec243e6d5ec57918b569bd94189fc616fc
sbx_user1051 <sbx_user1051@169.254.33.185>
2022-05-26 09:57:20 +0000

    Thu May 26 09:57:20 UTC 2022

--- solutions/sum_r1.py
+++ solutions/sum_r1.py
@@ -9,8 +9,3 @@ def sum_two_integer(input_one, input_two):
         logging.error("Input is not a integer")
     if 0 <= input_one <= 100 and 0 <= input_two <= 100:
         return sum(input_one, input_two)
-
-
-
-
-
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,5 +4,3 @@ from solutions.sum_r1 import sum_two_integer
 class TestSum():
     def test_sum(self):
         assert sum_two_integer(1, 2) == 3
-
-
