55d438aea4c09391283b9bf5e508ebc071e06821
sbx_user1051 <sbx_user1051@169.254.6.101>
2022-05-26 09:42:55 +0000

    Thu May 26 09:42:55 UTC 2022

--- solutions/sum_r1.py
+++ solutions/sum_r1.py
@@ -1,7 +1,14 @@
+import logging
 
 
-def sum_two_integer(input_one-> int, input_two):
+def sum_two_integer(input_one, input_two):
+    try:
+        int(input_one)
+        int(input_two)
+    except ValueError:
+        logging.ERROR(f"")
 
     pass
+
 
 
