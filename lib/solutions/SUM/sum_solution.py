b203a96aeed68959266805087d815c4b6ea7d382
sbx_user1051 <sbx_user1051@169.254.85.197>
2022-05-27 14:31:45 +0000

    Fri May 27 14:31:45 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+
+def validate_input_unsigned(input_value: int, max_value: int = 100):
+    return isinstance(input_value, int) and 0 <= input_value <= max_value
+
+
+def compute(input_one: int, input_two: int):
+    if validate_input_unsigned(input_one) and validate_input_unsigned(input_two):
+        return input_one + input_two
+    else:
+        raise ValueError("The input provided is not between 0 to 100")
+
