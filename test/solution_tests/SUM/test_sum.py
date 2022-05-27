a1d52ebb689c51e30cff3fd12d8357b56999aca9
sbx_user1051 <sbx_user1051@169.254.85.197>
2022-05-27 14:16:56 +0000

    Fri May 27 14:16:56 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,6 @@
-from solutions.sum_r1 import sum_two_integer
+from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
-        assert sum_two_integer(1, 2) == 3
+        assert sum_solution.compute(1, 2) == 3
--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,13 +1,6 @@
-import pytest
-
-from solutions.sum_r1 import sum_two_integer
+from solutions.TST import one
 
 
-class TestSum:
+class TestSum():
     def test_sum(self):
-        assert sum_two_integer(5, 100) == 105
-
-    def test_sum_failure(self):
-        with pytest.raises(ValueError):
-            sum_two_integer(1, None)
-
+        assert one.get() == 1
