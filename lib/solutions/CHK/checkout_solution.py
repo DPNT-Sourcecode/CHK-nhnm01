15f425a7a320afc729d5170658c787222ddb5401
sbx_user1051 <sbx_user1051@169.254.85.197>
2022-05-27 14:48:48 +0000

    Fri May 27 14:48:48 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
 
+price_map = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+special_offers = {
+    "A": {
+        "quantity": 3,
+        "price": 130,
+        "description": "3 for the price of 130"
+    },
+    "B": {
+        "quantity": 2,
+        "price": 45,
+        "description": "2 for the price of 45"
+    }
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+
+
