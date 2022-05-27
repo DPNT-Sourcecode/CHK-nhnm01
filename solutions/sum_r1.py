1486d212e1187a99e51ac60f93bc9255da73501e
sbx_user1051 <sbx_user1051@169.254.67.89>
2022-05-27 14:04:55 +0000

    Fri May 27 14:04:55 UTC 2022

--- solutions/sum_r1.py
+++ solutions/sum_r1.py
@@ -1,11 +1,10 @@
-import logging
+def validate_input_unsigned(input_value: int, max_value: int = 100):
+    return 0 <= input_value <= max_value
 
 
-def sum_two_integer(input_one, input_two):
-    try:
-        int(input_one)
-        int(input_two)
-    except ValueError:
-        logging.error("Input is not a integer")
-    if 0 <= input_one <= 100 and 0 <= input_two <= 100:
-        return sum(input_one, input_two)
+def sum_two_integer(input_one: int, input_two: int):
+    if validate_input_unsigned(input_one) and validate_input_unsigned(input_two):
+        return input_one + input_two
+    else:
+        raise ValueError("The input provided is not between 0 to 100")
+
